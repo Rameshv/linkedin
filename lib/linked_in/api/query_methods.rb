489420deda81fdb1507f33fbd67e862fee35b036
Rameshv <rameshv@offerbull.com>
2011-12-15 16:28:43 +0530

    network update fix

--- lib/linked_in/api/query_methods.rb
+++ lib/linked_in/api/query_methods.rb
@@ -15,33 +15,63 @@ module LinkedIn
 
       def network_updates(options={})
         path = "#{person_path(options)}/network/updates"
+        network_query(path, options)
+        #simple_query(path, options)
+      end
+
+      def company(options = {})
+        path = company_path(options)
         simple_query(path, options)
       end
 
       private
 
-        def simple_query(path, options={})
-          fields = options[:fields] || LinkedIn.default_profile_fields
+      def simple_query(path, options={})
+        fields = options[:fields] || LinkedIn.default_profile_fields
 
-          if options[:public]
-            path +=":public"
-          elsif fields
-            path +=":(#{fields.map{ |f| f.to_s.gsub("_","-") }.join(',')})"
-          end
-
-          Mash.from_json(get(path))
+        if options[:public]
+          path +=":public"
+        elsif fields
+          path +=":(#{fields.map { |f| f.to_s.gsub("_", "-") }.join(',')})"
         end
 
-        def person_path(options)
-          path = "/people/"
-          if options[:id]
-            path += "id=#{options[:id]}"
-          elsif options[:url]
-            path += "url=#{CGI.escape(options[:url])}"
-          else
-            path += "~"
-          end
+        Mash.from_json(get(path))
+      end
+
+      def network_query(path,options)
+        query_str = ''
+        options.each do |key, val|
+          query_str += ((query_str=='') ? '' : '&') + key.to_s + '=' + val.to_s
         end
+        path += '?' + query_str unless query_str == ''
+        Mash.from_json(get(path))
+      end
+
+      def person_path(options)
+        path = "/people/"
+        if options[:id]
+          path += "id=#{options[:id]}"
+        elsif options[:url]
+          path += "url=#{CGI.escape(options[:url])}"
+        else
+          path += "~"
+        end
+      end
+
+      def company_path(options)
+        path = "/companies/"
+        if options[:id]
+          path += "id=#{options[:id]}"
+        elsif options[:url]
+          path += "url=#{CGI.escape(options[:url])}"
+        elsif options[:name]
+          path += "universal-name=#{CGI.escape(options[:name])}"
+        elsif options[:domain]
+          path += "email-domain=#{CGI.escape(options[:domain])}"
+        else
+          path += "~"
+        end
+      end
 
     end
 
